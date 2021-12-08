d1645b6a5e7a0d2512ba8fa8b30b08bf2cc27f70
Akshansh20018 <akshansh20018@iiitd.ac.in>
2021-12-09 02:34:34 +0530

    Completed Home Page

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import javafx.animation.TranslateTransition;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.util.Duration;
@@ -18,23 +19,47 @@ public class HelloController implements Initializable {
     @FXML
     private ImageView boss;
 
+    @FXML
+    private ImageView cloud_1;
+
+    @FXML
+    private ImageView cloud_2;
+
+    @FXML
+    private ImageView cloud_3;
+
+    @FXML
+    private ImageView cloud_4;
+
+    public void vertical_jump(Node node, int n, boolean rev, int time) {
+        TranslateTransition t= new TranslateTransition();
+        t.setNode(node);
+        t.setByY(n);
+        t.setAutoReverse(rev);
+        t.setDuration(Duration.millis(time));
+        t.setCycleCount(TranslateTransition.INDEFINITE);
+        t.play();
+    }
+
+    public void hor_move(Node node, int n, int delay, boolean rev, int time) {
+        TranslateTransition t= new TranslateTransition();
+        t.setNode(node);
+        t.setByX(n);
+        t.setDelay(Duration.millis(delay));
+        t.setAutoReverse(rev);
+        t.setDuration(Duration.millis(time));
+        t.setCycleCount(TranslateTransition.INDEFINITE);
+        t.play();
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         //Hero translation
-        TranslateTransition translate_hero = new TranslateTransition();
-        translate_hero.setNode(hero);
-        translate_hero.setByY(-55);
-        translate_hero.setAutoReverse(true);
-        translate_hero.setDuration(Duration.millis(400));
-        translate_hero.setCycleCount(TranslateTransition.INDEFINITE);
-        translate_hero.play();
-
-        TranslateTransition translate_boss = new TranslateTransition();
-        translate_boss.setNode(boss);
-        translate_boss.setByY(-90);
-        translate_boss.setAutoReverse(true);
-        translate_boss.setDuration(Duration.millis(900));
-        translate_boss.setCycleCount(TranslateTransition.INDEFINITE);
-        translate_boss.play();
+        vertical_jump(hero, -55, true, 550);
+        vertical_jump(boss, -90, true, 900);
+        hor_move(cloud_1, -900, 1000, false, 12000);
+        hor_move(cloud_2, -1000, 5000, false, 12000);
+        hor_move(cloud_3, -990, 1000, false, 12000);
+        hor_move(cloud_4, -1100, 5000, false, 12000);
     }
 }
